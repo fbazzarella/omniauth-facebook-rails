696ef78744009a79eee3a6f23a13543acc1a31ad
Pedro Nascimento <pnascimento@gmail.com>
2012-12-04 13:10:33 -0200

    requiring omniauth and omniauth/facebook.

--- lib/omniauth-facebook-rails/engine.rb
+++ lib/omniauth-facebook-rails/engine.rb
@@ -4,6 +4,8 @@ module OmniauthFacebookRails
       g.test_framework :rspec, :view_specs => false
     end
     initializer "omniauth_facebok_rails.add_facebook" do |app|
+      require 'omniauth'
+      require 'omniauth/facebook'
       app.middleware.use OmniAuth::Builder do
         provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], :scope => "email"
       end
