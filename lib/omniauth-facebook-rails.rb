dad6b91138fac6a9643f3f8bf7910a33e351bd19
Pedro Nascimento <pnascimento@gmail.com>
2012-12-04 13:25:58 -0200

    requiring factory girl in test.

--- lib/omniauth-facebook-rails.rb
+++ lib/omniauth-facebook-rails.rb
@@ -1,4 +1,8 @@
 require "omniauth-facebook-rails/engine"
+if Rails.env.to_s == "test"
+  require 'factory_girl'
+  require File.expand_path('../../spec/factories', __FILE__)
+end
 
 module OmniauthFacebookRails
 end
