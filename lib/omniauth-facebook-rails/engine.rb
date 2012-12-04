da6188dadacc9f68968a26ecc55e0a135a40745b
Pedro Nascimento <pnascimento@gmail.com>
2012-12-04 17:58:25 -0200

    Adding methods we would like to use in app controller.

--- lib/omniauth-facebook-rails/engine.rb
+++ lib/omniauth-facebook-rails/engine.rb
@@ -10,5 +10,25 @@ module OmniauthFacebookRails
         provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], :scope => "email"
       end
     end
+    ActiveSupport.on_load(:action_controller) do
+      helper_method :current_user, :user_signed_in?
+      def save_return_path
+        session[:return_path] = request.path
+      end
+      def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      rescue ActiveRecord::RecordNotFound
+        session.delete(:user_id)
+        nil
+      end
+
+      def user_signed_in?
+        !current_user.nil?
+      end
+
+      def authenticate!
+        user_signed_in? || redirect_to((request.env["HTTP_REFERER"].blank?) ? root_path : :back, notice: "Voce precisa estar logado...")
+      end
+    end
   end
 end
