cedcb2ab9975a86d04f519567efec66c78489dd5
Eduardo Fiorezi <eduardofiorezi@gmail.com>
2013-01-14 22:18:05 -0200

    Add namespace to User model
    Add expected file

--- app/models/omniauth_facebook_rails/user.rb
+++ app/models/omniauth_facebook_rails/user.rb
@@ -1,4 +1,6 @@
-class User < ActiveRecord::Base
+class OmniauthFacebookRails::User < ActiveRecord::Base
+  self.table_name = "users"
+  
   def self.find_or_create_with_omniauth(auth)
     user = self.find_or_create_by_provider_and_uid(auth["provider"], auth["uid"])
     user.assign_attributes({:name => auth["info"]["name"], :email => auth["info"]["email"], :photo_url => auth["info"]["image"], :access_token => auth["credentials"]["token"], :extra => (auth["extra"]["raw_info"] rescue nil)}, :without_protection => true)
