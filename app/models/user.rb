0e18d051ac7a95e1501efee321badc1751e81f44
Felipe Bazzarella <fbazzarella@gmail.com>
2013-06-19 15:52:06 -0300

    Assign user's location from Facebook.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   def self.find_or_create_with_omniauth(auth)
     user = self.find_or_create_by_provider_and_uid(auth["provider"], auth["uid"])
-    user.assign_attributes({:name => auth["info"]["name"], :email => auth["info"]["email"], :photo_url => auth["info"]["image"], :access_token => auth["credentials"]["token"], :extra => (auth["extra"]["raw_info"] rescue nil)}, :without_protection => true)
+    user.assign_attributes({:name => auth["info"]["name"], :email => auth["info"]["email"], :photo_url => auth["info"]["image"], :location => auth["info"]["location"], :access_token => auth["credentials"]["token"], :extra => (auth["extra"]["raw_info"] rescue nil)}, :without_protection => true)
     user.save(validate: false)
     user
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe User do
   describe ".find_or_create_with_omniauth" do
-    let(:auth) { {"provider"=>"facebook", "uid"=>"123456", "credentials"=>{"token"=>"AAADv"}, "info"=>{"nickname"=>"sylvestre.mergulhao", "email"=>"mergulhao83@gmail.com", "first_name"=>"Sylvestre", "last_name"=>"Mergulhão", "name"=>"Sylvestre Mergulhão", "image"=>"http://graph.facebook.com/526830634/picture?type=square", "urls"=>{"Facebook"=>"http://www.facebook.com/sylvestre.mergulhao", "Website"=>nil}}, "extra"=>{"raw_info"=>{"id"=>"526830634", "name"=>"Sylvestre Mergulhão", "first_name"=>"Sylvestre", "last_name"=>"Mergulhão", "link"=>"http://www.facebook.com/sylvestre.mergulhao", "username"=>"sylvestre.mergulhao", "location"=>{"id"=>"110346955653479", "name"=>"Rio de Janeiro, Rio de Janeiro"}, "education"=>[{"school"=>{"id"=>"115555431792035", "name"=>"UniRio"}, "type"=>"College", "with"=>[{"id"=>"100001030509286", "name"=>"João Aguirre"}, {"id"=>"512001183", "name"=>"Marcos Brando Margulies"}, {"id"=>"100001650369677", "name"=>"Paulo Guilherme Ribeiro"}, {"id"=>"678564390", "name"=>"Duim Knurf"}]}, {"school"=>{"id"=>"109896575694939", "name"=>"Universidade Federal do Estado do Rio de Janeiro"}, "type"=>"College", "with"=>[{"id"=>"100000517388839", "name"=>"João Filho"}]}], "gender"=>"male", "email"=>"mergulhao83@gmail.com", "timezone"=>-2, "locale"=>"pt_BR", "verified"=>true, "updated_time"=>"2011-07-19T00:05:20+0000"}}} }
+    let(:auth) { {"provider"=>"facebook", "uid"=>"123456", "credentials"=>{"token"=>"AAADv"}, "info"=>{"nickname"=>"sylvestre.mergulhao", "email"=>"mergulhao83@gmail.com", "first_name"=>"Sylvestre", "last_name"=>"Mergulhão", "name"=>"Sylvestre Mergulhão", "image"=>"http://graph.facebook.com/526830634/picture?type=square", "location"=>"Rio de Janeiro, Rio de Janeiro", "urls"=>{"Facebook"=>"http://www.facebook.com/sylvestre.mergulhao", "Website"=>nil}}, "extra"=>{"raw_info"=>{"id"=>"526830634", "name"=>"Sylvestre Mergulhão", "first_name"=>"Sylvestre", "last_name"=>"Mergulhão", "link"=>"http://www.facebook.com/sylvestre.mergulhao", "username"=>"sylvestre.mergulhao", "location"=>{"id"=>"110346955653479", "name"=>"Rio de Janeiro, Rio de Janeiro"}, "education"=>[{"school"=>{"id"=>"115555431792035", "name"=>"UniRio"}, "type"=>"College", "with"=>[{"id"=>"100001030509286", "name"=>"João Aguirre"}, {"id"=>"512001183", "name"=>"Marcos Brando Margulies"}, {"id"=>"100001650369677", "name"=>"Paulo Guilherme Ribeiro"}, {"id"=>"678564390", "name"=>"Duim Knurf"}]}, {"school"=>{"id"=>"109896575694939", "name"=>"Universidade Federal do Estado do Rio de Janeiro"}, "type"=>"College", "with"=>[{"id"=>"100000517388839", "name"=>"João Filho"}]}], "gender"=>"male", "email"=>"mergulhao83@gmail.com", "timezone"=>-2, "locale"=>"pt_BR", "verified"=>true, "updated_time"=>"2011-07-19T00:05:20+0000"}}} }
     context "user doesnt exists" do
       describe "creating a new user" do
         it "should create" do
@@ -20,6 +20,7 @@ describe User do
         it { subject.email.should == "mergulhao83@gmail.com" }
         it { subject.access_token.should == "AAADv" }
         it { subject.photo_url.should == "http://graph.facebook.com/526830634/picture?type=square" }
+        it { subject.location.should == "Rio de Janeiro, Rio de Janeiro" }
         it { subject.extra.should_not be_nil }
       end
     end
@@ -46,6 +47,7 @@ describe User do
         it { subject.email.should == "mergulhao83@gmail.com" }
         it { subject.access_token.should == "AAADv" }
         it { subject.photo_url.should == "http://graph.facebook.com/526830634/picture?type=square" }
+        it { subject.location.should == "Rio de Janeiro, Rio de Janeiro" }
       end
     end
   end
